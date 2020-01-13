7ecc444cea1ca66d7bcf55bd990ebcd8b5255b05
Ibrahim Kholy <ibrahem9311@yahoo.com>
2020-01-13 23:41:43 +0200

    create canvas element, Initiate the game with hero, game loop and user input handler.
    - The first thing we need to do is create a canvas element,Once we have the element we get a reference to its context, which we use to issue drawing commands. Then we set its dimensions, and add it to document so it'll appear in the page.
    - we just want to store the user input for later instead of acting on it immediately.
    - the update function and is called every single interval execution. The first thing it does is checks the up, down, left, and right arrow keys to see if the user has pressed them. If so, the hero is moved in the corresponding direction.
    - The main game loop is what controls the flow of the game. First we want to get the current timestamp so we can calculate the delta (how many milliseconds have passed since the last interval).

--- js/classes.js
+++ js/classes.js
@@ -1,1 +1,75 @@
 /*****************Used Classes*****************/
+
+//class hero 
+class player {
+    constructor(heroImgSrc, game_Width, game_height) {
+        this.heroImage = new Image();
+        this.heroImage.src = heroImgSrc
+        this.game_Width = game_Width;
+        this.width = playerWidth;
+        this.height = playerHeight;
+        this.maxspeed = player_MaxSpeed;
+        this.speed = 0     //current speed
+
+        this.position = {
+            x: game_Width / 2 - this.width / 2,
+            y: game_height - 10
+        };
+    }
+
+    draw() {
+        context.drawImage(this.heroImage, this.position.x, this.position.y, this.width, this.height);
+    }
+
+    clear() {
+        context.clearRect(this.position.x - 10, this.position.y, this.width + 20, this.height)
+    }
+    // update every 5 pixels per time passed 
+    update(deltaTime) {
+        if (!deltaTime) return;
+        this.position.x += this.speed;
+        if (this.position.x < 0) this.position.x = 0;
+        if (this.position.x + this.width > game_Width) {
+            this.position.x = this.game_Width - this.width;
+        }
+    }
+    // move left 
+    moveleft() { this.speed = -this.maxspeed; }
+    // move right
+    moveRight() { this.speed = this.maxspeed; }
+    //  stop hero
+    stop() { this.speed = 0; }
+
+}
+
+// user input handler 
+class inputHandler {
+    constructor(hero) {
+        document.addEventListener('keydown', event => {
+            switch (event.keyCode) {
+                case 37:
+                    hero.moveleft();
+                    break;
+                case 39:
+                    hero.moveRight();
+                    break;
+            }
+        })
+
+        document.addEventListener('keyup', event => {
+            switch (event.keyCode) {
+                case 37:
+                    if (hero.speed < 0) {
+                        hero.stop();
+                    }
+                    break;
+                case 39:
+                    if (hero.speed > 0) {
+                        hero.stop();
+                    }
+                    break;
+            }
+        })
+
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,46 @@
-/*****************Variables*****************/
+/***************** Variables *****************/
+var playerWidth = 150;
+var playerHeight = 150;
+var player_MaxSpeed = 10;
+var lastTime = 0;
+var heroImageSrc = "img/ship.png";
+var hero;
 
 
+/***************** Constants *****************/
+const game_Width = 1024;
+const game_height = 400;
+const canvas_Width = 1024;
+const canvas_height = 600;
 
-/*****************Constants*****************/
+/*************** create canvas *************/
+
+var canvas = document.getElementById("myCanvas");
+canvas.width = canvas_Width;
+canvas.height = canvas_height;
+var context = canvas.getContext("2d");// call all of the rendering APIs
+
+/***************** man functions ************/
+
+//init the game
+function init() {
+    hero = new player(heroImageSrc, game_Width, game_height);
+    hero.draw();
+    new inputHandler(hero);
+}
+
+// game loop
+function gameLoop(timeStamp) {
+    //get how many milliseconds have passed since the last interval
+    var deltaT = timeStamp - lastTime;
+    lastTime = timeStamp;
+    hero.update(deltaT);
+    hero.clear();
+    hero.draw(context);
+    // Request to do this again
+    requestAnimationFrame(gameLoop);
+}
+
+/**************** calling functions  ***********/
+init();
+gameLoop();
