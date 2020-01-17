69894301b592fe9340753d0b6ef2b23bf3029553
Mohamed Brikaa <MohamedBrikaa@users.noreply.github.com>
2020-01-17 14:45:30 +0200

    update variables, init and loop functions

--- js/main.js
+++ js/main.js
@@ -6,13 +6,6 @@ const canvas_height = 600;
 const bulletWidth=8;
 const bulletHeight=40;
 const bulletStep=5;
-const enemyWidth=40;
-const enemyHeight=30;
-const enemylivesNum=1
-const enemyNum=40
-const verEnemiesMargin=10;
-const horEnemiesMargin=20;
-
 /***************** Variables *****************/
 var playerWidth = 150;
 var playerHeight = 150;
@@ -24,19 +17,22 @@ var spacePressed=false;
 var heroBullets = [];
 var enemyBullets = [];
 
-
+var count = 0;
+var fireSpeed = 0;
+var score;
+var totalScore = 0;
+var lives = 50;
 var enemyArray = [];
 var steps;
 var gameON= true;
 var enemyImageSrc = "img/enemy.png";
-var steps=Math.floor( (canvas_Width-((enemyNum/4)*enemyWidth + 9*horEnemiesMargin)) /enemyMoveStep);
-
+var right = true;
 
 var progress={
     lvl:1,
     stage:1
 }
-var enemyImgSrcArray=["",""]
+var enemyImgSrcArray = ["", "img/BigChickenCIU.png","img/BigChicken.png"]
 var enemies={
     imgSrc:enemyImgSrcArray[progress.lvl],
     enemyMoveStep:3,
@@ -63,23 +59,50 @@ resize(); //resize call function
 
 //init the game
 function init() {
-    hero = new player(heroImageSrc, game_Width, game_height);
-    hero.draw();
-    new inputHandler(hero);
-    creatEnemyArray(enemyArray,enemyImageSrc);
+    score = 0;
+    if (progress.lvl == 1 && progress.stage == 1) {
+        window.addEventListener('load', resize, false);
+        window.addEventListener('resize', resize, false);
+        hero = new player(heroImageSrc, game_Width, game_height);
+        hero.draw();
+        new inputHandler(hero);
+        creatEnemyArray();
+    }
+    else if(progress.lvl<4){
+        count = 0;
+        right = true;
+        enemies.imgSrc=enemyImgSrcArray[progress.stage]
+        enemies.enemylivesNum= (progress.lvl-1);
+        enemies.numberOfLines =  progress.stage*2;
+        enemies.enemyNum = enemies.numberOfLines*10;
+        creatEnemyArray();
+    }else{
+        document.getElementsByClassName("progress")[0].innerHTML = `<h1>Congratulation !!</h1><h1>Total score is : ${totalScore}</h1>`
+        document.getElementsByClassName("progress")[0].style.background = "#4caf5063";
+        document.getElementsByClassName("progress")[0].style.display = "block";
+    }
 }
 
 // game loop
 function gameLoop(timeStamp) {
     //get how many milliseconds have passed since the last interval
     var deltaT = timeStamp - lastTime;
-    lastTime = timeStamp;
+    moveEnemies();
+    if (fireSpeed == 20) {
+        enemyFire();
+        fireSpeed = 0;
+    }
+    else {
+        fireSpeed++;
+    }
+    moveEnemyBullets();
+    moveHeroBullets();
     hero.update(deltaT);
     hero.clear();
-    hero.draw(context);
-    moveEnemies();
-    moveHeroBullets();
-    moveEnemyBullets();
+    hero.draw();
+    if (!enemyArray.length) {
+        nextStage();
+    }
     // Request to do this again
     requestAnimationFrame(gameLoop);
     
@@ -88,9 +111,6 @@ function gameLoop(timeStamp) {
 /**************** calling functions  ***********/
 init();
 gameLoop();
-
-
-
 /**************** Menu Buttons  ***********/
 
 //pause/resume button
